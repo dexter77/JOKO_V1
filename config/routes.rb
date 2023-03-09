589c5707c0bc6a5a3e22b49d8192c32fdc1e4357
dexter77 <gueye.tamsir78@gmail.com>
2023-03-09 07:06:27 +0100

    homepage && registration page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
 
   root to: "users#sign_in"
+  post "users/sign_in", to: "users#create"
   resources :users, only: [:new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
